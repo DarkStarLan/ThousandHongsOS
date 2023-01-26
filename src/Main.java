0b1762ed61af0fc1fadc00b175665dc5c5207ffa
DarkStarLan <1621582607@qq.com>
2023-01-26 18:45:39 +0800

    006

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import Main.isFlyable;
 public class Main {
     public static void main(String[] args) {
         String str = "D:/Hello/abc.html";
-        System.out.println(Path.GetFileName(str));
+        System.out.println(Path.GetFileNameWithType(str));
         System.out.println("Hello world!");
         System.out.println("Hello Git");
     }
--- src/Main/Path.java
+++ src/Main/Path.java
@@ -1,10 +1,14 @@
 package Main;
 
 public class Path {
-    public static String GetFileName(String Load) {
+    public static String GetFileNameWithoutType(String Load) {
         int pos = Load.lastIndexOf('.');
         if (pos == -1) return "";
         int slash_pos = Load.lastIndexOf('/');
-        return Load.substring(slash_pos == -1 ? 0 : slash_pos, pos);
+        return Load.substring(slash_pos == -1 ? 0 : slash_pos + 1, pos);
+    }
+    public static String GetFileNameWithType(String Load) {
+        int slash_pos = Load.lastIndexOf('/');
+        return Load.substring(slash_pos == -1 ? 0 : slash_pos + 1, Load.length() - slash_pos);
     }
 }
